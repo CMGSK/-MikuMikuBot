b3fd8748494107e689bd8e379e61c14520c35694
Carlos <carlosmgov7k@gmail.com>
2023-03-11 18:38:59 +0100

    minor advances

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,83 @@
 import * as booru from 'booru'
 import { CronJob } from 'cron';
 import fs from 'fs';
-import generator, { Entity, Response } from 'megalodon';
+import generator, { Entity, NotificationType, Response, WebSocketInterface } from 'megalodon';
 import https from 'https';
 
 const BASE_URL: string = 'https://botsin.space'
+const WSS_BASE_URL: string = 'wss://botsin.space'
 const access_token: string = 'ftKpSn_psozTAjTwlpw5DS6rS4n7ukqs1pUbjybCDuQ'
 const client = generator('mastodon', BASE_URL, access_token)
-// const toot: string = 'miku'
+const WSSclient = generator('mastodon', WSS_BASE_URL, access_token)
+const toot: string = 'miku'
 const image = fs.createReadStream('pics/4245447.jpg')
+const regex = new RegExp('(?<=!)\w+') // (?<=!) possitive lookahead assertion
+// var lastNotifID = '';
+// const excludes: Array<string> = [
+//     NotificationType.Follow,
+//     NotificationType.Favourite,
+//     NotificationType.Reblog,
+//     NotificationType.PollVote,
+//     NotificationType.PollExpired,
+//     NotificationType.EmojiReaction,
+// ]
+
+//Miku image every 30min
+// let mikuJob = new CronJob('*/30 * * * *', mikuSearch, null, true, 'Europe/Madrid');
+async function mikuSearch(){
+    booru.search('safebooru', ['hatsune_miku'], { limit: 1, random: true})
+        .then( pics => {
+            const pic = pics[0];
+            https.get(pic.fileUrl!, imageResponse => 
+                client.uploadMedia(imageResponse)
+                .then((uploadResponse: Response<Entity.Attachment | Entity.AsyncAttachment>) => {
+                    console.log(uploadResponse.data)
+                    // client.postStatus(`Tags: ${pic.tags.slice(0,10).join(', ')}\nPost: ${pic.postView}`, {media_ids: [uploadResponse.data.id], sensitive: true})
+                    client.postStatus(`Miku time!`, {media_ids: [uploadResponse.data.id], sensitive: true})
+                        .then((postResponse: Response<Entity.Status>) => {
+                            console.log(postResponse.data);
+                        })
+                })
+            )
+        })
+}
+
+const stream: WebSocketInterface = WSSclient.userSocket()
+stream.on('notification', (notification: Entity.Notification) => {
+    // console.log(`New notification:\n${JSON.stringify(notification, null, 4)}`);
+    let mention: string = '' + notification.status?.content;
+    if (regex.test(mention)){
+        mention += '!hatusne_miku';
+        const addedTags = mention.match(regex);
+        // console.log(addedTags)
+        booru.search('safebooru', addedTags!, { limit: 1, random: true})
+            .then( pics => {
+                const pic = pics[0];
+                https.get(pic.fileUrl!, imageResponse => 
+                    client.uploadMedia(imageResponse)
+                    .then((uploadResponse: Response<Entity.Attachment | Entity.AsyncAttachment>) => {
+                        console.log(uploadResponse.data)
+                        client.postStatus(`Miku with ${addedTags}\n`, {in_reply_to_id: notification.id, media_ids: [uploadResponse.data.id], sensitive: true})
+                    })
+                )
+            })
+        }
+    })
+    //post the notification data JSON formatted
+    // console.log(`New notification:\n${JSON.stringify(notification, null, 4)}`);
+// })
+
+// Overkill notification reader lol
+// let notiJob = new CronJob('*/1 * * * *', getNotifs, null, true, 'Europe/Madrid');
+// async function getNotifs() {
+//     client.getNotifications({limit: 1, exclude_types: excludes})
+//         .then(Notif => {
+//             if (Notif.data[0].id != lastNotifID){
+//                 lastNotifID = Notif.data[0].id;
+//                 console.log(`New notification:\n${Notif.data[0]}`);
+//             }
+//         })
+// }
 
 // async function mikuSearch() {
 //     const posts = await booru.search('safebooru', ["hatsune_miku"], { limit: 1, random: true });
@@ -18,23 +87,6 @@ const image = fs.createReadStream('pics/4245447.jpg')
 //     }
 // }
 
-booru.search('safebooru', ['hatsune_miku'], { limit: 1, random: true})
-    .then( pics => {
-        const pic = pics[0]
-        https.get(pic.fileUrl!, imageResponse => 
-            client.uploadMedia(imageResponse)
-            .then((uploadResponse: Response<Entity.Attachment | Entity.AsyncAttachment>) => {
-                console.log(uploadResponse.data)
-                client.postStatus(`Tags: ${pic.tags.slice(0,10).join(', ')}\nPost: ${pic.postView}`, {media_ids: [uploadResponse.data.id], sensitive: true})
-                    .then((postResponse: Response<Entity.Status>) => {
-                        console.log(postResponse.data)
-                    })
-            })
-        )
-    })
-
-
-
 // uploads an image from its url
 // https.get('https://www.nippon.com/es/ncommon/contents/japan-topics/560508/560508.jpg', res => 
 //     client.uploadMedia(res)
@@ -42,7 +94,6 @@ booru.search('safebooru', ['hatsune_miku'], { limit: 1, random: true})
 //         console.log(res.data)
 //     })
 // )
-
 
 // Post an image
 // const client = generator('mastodon', BASE_URL, access_token)
@@ -55,18 +106,12 @@ booru.search('safebooru', ['hatsune_miku'], { limit: 1, random: true})
 //             })
 //     })
 
-
-
-
 //Post tweet
 // const client = generator('mastodon', BASE_URL, access_token)
 // client.postStatus(toot)
 //     .then((res: Response<Entity.Status>) => {
 //         console.log(res.data)
 //     })
-
-
-
 
 //Download a Miku img evry 10sec
 // let mikuJob = new CronJob('*/10 * * * * *', mikuSearch, null, true, 'Europe/Madrid');
